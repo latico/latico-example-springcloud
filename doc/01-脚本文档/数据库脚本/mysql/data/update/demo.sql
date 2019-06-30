fd9a48508f97acc7a20c976380dbc660a2f41802
landingdong <landingdong@gdcattsoft.com>
2019-06-30 18:58:44 +0800

    优化

--- doc/01-脚本文档/数据库脚本/mysql/data/update/demo.sql
+++
@@ -1,4 +0,0 @@
-
-insert  into `demo`(`id`,`username`,`password`,`administrator`) values (1,'name1','pass1',1),(2,'name2','pass2',0),(3,'名字3','密码3',1);
-
-INSERT  INTO `demo2`(`id`,`username`,`password`,`administrator`) VALUES (1,'name1','pass1',1),(2,'name2','pass2',0),(3,'名字3','密码3',1);
